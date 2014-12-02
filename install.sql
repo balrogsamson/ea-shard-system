c9e2712e3add76cd74967db6b0add9fb2bd8efbe
Peter Derouen <p.j.derouen@gmail.com>
2014-12-01 21:16:36 -0800

    Update install.sql
    Let's not get ahead of ourselves. What do we need a completion log for right now?

--- install.sql
+++ install.sql
@@ -1,25 +1,13 @@
 --
 -- Table structure for shard items.
+-- Tracks shard creation and leveling.
 --
 CREATE TABLE IF NOT EXISTS 'shard' (
     'id' int(11) unsigned NOT NULL autoincrement,
-    'creator_id' int(11) unsigned NOT NULL default '0',
-    'creator_name' varchar(30) NOT NULL default '',
-    'date_created' datetime NOT NULL default '0000-00-00 00:00:00'
+    'char_id' int(11) unsigned NOT NULL default '0',
     'item_id' int(11) unsigned NOT NULL default '1';
     'level' tinyint(2) NOT NULL default '1',
     'experience' int(4) NOT NULL default '0'
+    'date_created' datetime NOT NULL default '0000-00-00 00:00:00'
     PRIMARY KEY('id')
-);
-
-
---
--- Table structure for crystal items.
--- Completed crystal data is stored here.
---
-CREATE TABLE IF NOT EXISTS 'shard_log' (
-    'completion_date' datetime NOT NULL default '0000-00-00 00:00:00',
-    'char_id' int(11) unsigned NOT NULL default '0',
-    'char_name' varchar(30) NOT NULL default '',
-    'item_id' int(11) NOT NULL default '0'
 );
