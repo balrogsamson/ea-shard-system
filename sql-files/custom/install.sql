39c704e6e01d061a26f47bd9b6b5e989cb10699b
Peter Derouen <p.j.derouen@gmail.com>
2014-12-02 18:47:15 -0800

    Update install.sql

--- sql-files/custom/install.sql
+++ sql-files/custom/install.sql
@@ -2,10 +2,12 @@
 -- Table structure for shard items.
 -- Tracks shard creation and leveling.
 --
-CREATE TABLE IF NOT EXISTS 'shard' (
+CREATE TABLE IF NOT EXISTS 'di_shard_log' (
     'id' int(11) unsigned NOT NULL autoincrement,
     'char_id' int(11) unsigned NOT NULL default '0',
+    'char_name' varchar(30) NOT NULL default '',
     'item_id' int(11) unsigned NOT NULL default '1';
+    'item_name' varchar(30) NOT NULL default '',
     'level' tinyint(2) NOT NULL default '1',
     'experience' int(4) NOT NULL default '0'
     'date_created' datetime NOT NULL default '0000-00-00 00:00:00'
