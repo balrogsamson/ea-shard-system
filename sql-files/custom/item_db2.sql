e35048579fb7c48feb6322c6a2a2eaa092b3b069
Peter Derouen <p.j.derouen@gmail.com>
2014-12-03 17:34:24 -0800

    Create item_db2.sql
    Stores the crystal items into the database. I don't have time to finish this now.

---
+++ sql-files/custom/item_db2.sql
@@ -0,0 +1,3 @@
+# Dynamic Items - Shards
+# =============================================================
+#REPLACE INTO `item_db2` VALUES (30000,'Blazing_Shard_LV1','Blazing Shard LV1',20,NULL,NULL,
