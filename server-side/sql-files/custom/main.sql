1ac02b4f9ad0fb7bf3472e0a766ff1cf596d41ba
Peter Derouen <p.j.derouen@gmail.com>
2014-12-23 20:42:37 -0800

    Update main.sql
    Too cold in the main room. Moving to tablet!

--- server-side/sql-files/custom/main.sql
+++ server-side/sql-files/custom/main.sql
@@ -1,17 +1,18 @@
---
--- Table structure for 'shard_db'
---
-CREATE TABLE IF NOT EXISTS 'shard_db' (
-    'id' int(11) unsigned NOT NULL autoincrement,
-    'item_id' int(11) unsigned NOT NULL default '1';
-    'item_name' varchar(30) NOT NULL default '',
-    'char_id' int(11) unsigned NOT NULL default '0',
-    'char_name' varchar(30) NOT NULL default '',
-    'level' tinyint(2) NOT NULL default '1',
-    'experience' int(4) NOT NULL default '0'
-    'date_created' datetime NOT NULL default '0000-00-00 00:00:00'
-    PRIMARY KEY('id')
+
+CREATE TABLE IF NOT EXISTS `shard_db` (
+    `id` int(11) unsigned NOT NULL autoincrement,
+    `char_id` int(11) unsigned NOT NULL default '0',
+    `char_name` varchar(30) NOT NULL default '',
+    `item_id` int(11) unsigned NOT NULL default '1',
+    `item_name` varchar(30) NOT NULL default '',
+    `level` tinyint(2) NOT NULL default '1',
+    `experience` int(4) NOT NULL default '0',
+    `date_created` datetime NOT NULL default '0000-00-00 00:00:00',
+    PRIMARY KEY(`id`)
 ) ENGINE=MyISAM;
 
--- Sample entry
--- INSERT INTO 'shard_db' VALUES ('0', '15000', 'Placeholder Shard Lv1', '0', 'Placeholder PC', '1', '0', '0000-00-00 00:00:00');
+# Dynamic Items Database
+# Structure:
+#REPLACE INTO `item_db2` VALUES ( ID,'Name','Name',Type,Price,Sell,Weight,ATK,DEF,Range,Slot,Job,Class,Gender,Loc,wLV,eLV,Refineable,View,'Script','OnEquip_Script','OnUnequip_Script');
+
+REPLACE INTO `item_db2` VALUES (30000,'','THG Membership',3,NULL,10,10,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
